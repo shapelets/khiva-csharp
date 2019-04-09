af374876c31b96fc56f53acb5cd80a89fa4278e6
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 12:40:22 +0200

    Changed build_and_test_dotnet

--- .CI/travis/build_and_test_dotnet.sh
+++ .CI/travis/build_and_test_dotnet.sh
@@ -4,6 +4,5 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-dotnet restore KhivaCsharp/KhivaCsharp.sln
 dotnet build /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
 dotnet test ./KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll
