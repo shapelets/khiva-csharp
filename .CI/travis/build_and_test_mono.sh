a2f7a59d2ee2be1b008705ce2e21e6f33b8f7845
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 10:36:35 +0200

    Added nuget

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -4,6 +4,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+nuget restore KhivaCsharp/KhivaCsharp.sln
 msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
 mono ./KhivaCsharp/packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Debug/KhivaCSharpNUnitTest.dll -output:"coverage.xml" -filter:"+[*]khiva.*"
