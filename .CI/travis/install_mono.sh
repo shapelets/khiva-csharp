46a4ab5be244d1615c4880d14410027c3bd0a48f
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 12:36:14 +0200

    Changed path

--- .CI/travis/install_mono.sh
+++ .CI/travis/install_mono.sh
@@ -5,7 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 wget --no-check-certificate https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-PATH=/usr/local/bin:$PATH
-echo $LD_LIBRARY_PATH
-nuget.exe restore KhivaCsharp/KhivaCsharp.sln
+PATH=/usr/local/bin:/usr/lib$PATH
+nuget restore KhivaCsharp/KhivaCsharp.sln
 mono ./nuget.exe install NUnit.Runners -Version 3.0.1
