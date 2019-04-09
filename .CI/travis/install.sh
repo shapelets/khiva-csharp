536f701b8ded7343ae778e0cb9d800bca086956e
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 11:39:53 +0200

    Changed installation of codecov

--- .CI/travis/install.sh
+++ .CI/travis/install.sh
@@ -4,8 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+wget --no-check-certificate https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 PATH=/usr/local/bin:$PATH
 nuget restore KhivaCsharp/KhivaCsharp.sln
-curl -sSLO https://github.com/codecov/enterprise/releases/download/v4.1.3.zip
-unzip v4.1.3.zip && rm v4.1.3.zip && mv ./linux/codecov ./
+mono ./nuget.exe install NUnit.Runners -Version 3.0.1
