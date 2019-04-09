6d5a36e508c713958918725af9eeeb802f7a5fc1
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 12:50:32 +0200

    Changed paths

--- .CI/travis/install_dotnet.sh
+++ .CI/travis/install_dotnet.sh
@@ -4,7 +4,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-PATH=/usr/local/bin:/urs/lib/:$PATH
+PATH=/usr/local/bin:/usr/local/lib/:$PATH
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.2
