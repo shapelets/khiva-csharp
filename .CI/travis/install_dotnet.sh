6734696315101525aa9894c07fcb70bc57a57e96
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-09 12:17:11 +0200

    Added install dotnet

--- .CI/travis/install_dotnet.sh
+++ .CI/travis/install_dotnet.sh
@@ -5,4 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 PATH=/usr/local/bin:$PATH
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
 dotnet restore KhivaCsharp/KhivaCsharp.sln
