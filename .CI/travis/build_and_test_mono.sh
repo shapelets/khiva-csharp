8c2f69d52099b2b62f1d160b6e88a18afc74f8d7
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-10 13:30:35 +0200

    Added ls Mono.framework

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -5,5 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+ls Library/Frameworks/Mono.framework
 msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
 mono ./testrunner/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll
