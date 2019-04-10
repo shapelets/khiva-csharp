070a50368552c4ec0606a42795fa8e4448f40ed1
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-10 13:19:37 +0200

    Fixed dll path

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -6,4 +6,4 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
-mono ./testrunner/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Debug/KhivaCSharpNUnitTest.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll
