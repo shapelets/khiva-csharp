ad9a58affdecde0c923d3a77f606a9e2fc891dff
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-10 11:27:00 +0200

    Changed msbuild to dotnet msbuild

--- .CI/travis/build_and_test_dotnet.sh
+++ .CI/travis/build_and_test_dotnet.sh
@@ -5,5 +5,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 dotnet restore
-msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
+dotnet msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
 dotnet test ./KhivaCsharp/KhivaCSharpNUnitTest/KhivaCSharpNUnitTest.csproj
