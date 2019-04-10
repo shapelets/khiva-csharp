330d2e04b87065c440e6ab0f9e5352a13302127d
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-10 11:54:18 +0200

    Added .exe termination to OpenCover

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -8,4 +8,4 @@
 nuget restore KhivaCsharp/KhivaCsharp.sln
 msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
 ls ./KhivaCsharp/packages/OpenCover.4.6.519/tools
-./KhivaCsharp/packages/OpenCover.4.6.519/tools/OpenCover.Console -register:user -target:"./KhivaCsharp/packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe" -targetargs:"KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Debug/KhivaCSharpNUnitTest.dll" -output:"coverage.xml" -filter:"+[*]khiva.*"
+./KhivaCsharp/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe -register:user -target:"./KhivaCsharp/packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe" -targetargs:"KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Debug/KhivaCSharpNUnitTest.dll" -output:"coverage.xml" -filter:"+[*]khiva.*"
