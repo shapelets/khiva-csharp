ae6f1d028dcd352d97479a58c43eb72597d3ae2c
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-11 10:57:36 +0200

    Added some sudo

--- .CI/travis/install_dotnet.sh
+++ .CI/travis/install_dotnet.sh
@@ -5,4 +5,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-dotnet restore KhivaCsharp/KhivaCsharp.sln
+sudo dotnet restore KhivaCsharp/KhivaCsharp.sln
