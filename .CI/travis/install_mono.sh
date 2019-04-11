6bd2f0511161d92a5220aabe582105db0f568706
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-11 10:46:56 +0200

    Changed Paths

--- .CI/travis/install_mono.sh
+++ .CI/travis/install_mono.sh
@@ -5,8 +5,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-PATH=/usr/local/bin:/usr/lib:$PATH
-LD_LIBRARY_PATH=/usr/local/lib:/usr/local/include:$LD_LIBRARY_PATH
-DYLD_FALLBACK_LIBRARY_PATH=/usr/local/include:$DYLD_FALLBACK_LIBRARY_PATH
 nuget restore KhivaCsharp/KhivaCsharp.sln
 nuget install NUnit.ConsoleRunner -Version 3.9.0 -OutputDirectory ./testrunner
