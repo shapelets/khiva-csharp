cba88e2a6476b34ddd67603ab68f0293ea1c016f
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-11 11:24:42 +0200

    Added a printenv

--- .CI/travis/install_dotnet.sh
+++ .CI/travis/install_dotnet.sh
@@ -5,4 +5,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-nuget restore KhivaCsharp/KhivaCsharp.sln
+printenv | grep DYLD
+dotnet restore KhivaCsharp/KhivaCsharp.sln
