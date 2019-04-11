c5f120ba36e89e9df2b42a2f2c5469a73340acf5
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-11 09:39:44 +0200

    Added env variables

--- .CI/travis/install_dotnet.sh
+++ .CI/travis/install_dotnet.sh
@@ -5,7 +5,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-PATH=/usr/local/bin:/usr/lib:$PATH
-LD_LIBRARY_PATH=/usr/local/lib:/usr/local/include:$LD_LIBRARY_PATH
-DYLD_FALLBACK_LIBRARY_PATH=/usr/local/include:$DYLD_FALLBACK_LIBRARY_PATH
 dotnet restore KhivaCsharp/KhivaCsharp.sln
