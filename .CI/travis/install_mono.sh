759bb0fe2952a1bae5feab08bd5daa1cbb050084
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-11 12:04:06 +0200

    Added travis_retry

--- .CI/travis/install_mono.sh
+++ .CI/travis/install_mono.sh
@@ -5,4 +5,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-nuget restore KhivaCsharp/KhivaCsharp.sln
+travis_retry nuget restore -source "https://www.nuget.org/api/v2" KhivaCsharp/KhivaCsharp.sln
