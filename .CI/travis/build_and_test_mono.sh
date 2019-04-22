6c3f3e9354b5bdd744f68ef86e8f493c05d89384
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-22 09:09:36 +0200

    Changed travis configuration for osx

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -6,7 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    msbuild /p:Configuration=Release KhivaCsharp/KhivaCsharp.sln
+    msbuild KhivaCsharp/KhivaCsharp.sln
     nunit-console KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Release/KhivaCSharpNUnitTest.dll
 else
     msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
