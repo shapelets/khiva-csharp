42fde3ab44cdf484daef79771a9d631d19aa3b6a
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-22 11:56:08 +0200

    nuget update nunit-console

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -6,8 +6,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    nuget update nunit-console
     msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
-    nunit3-console KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll
+    nunit-console KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll
 else
     msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
     mono ./KhivaCsharp/packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll --config=Debug
