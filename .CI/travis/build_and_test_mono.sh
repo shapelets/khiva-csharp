5b2b6eadc4e02e09f6b99104fd95d81c6995d630
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-22 09:29:58 +0200

    Changed build and test for osx

--- .CI/travis/build_and_test_mono.sh
+++ .CI/travis/build_and_test_mono.sh
@@ -6,8 +6,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    msbuild KhivaCsharp/KhivaCsharp.sln
-    nunit-console KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Release/KhivaCSharpNUnitTest.dll
+    msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
+    nunit-console KhivaCsharp/KhivaCSharpNUnitTest/bin/x64/Debug/KhivaCSharpNUnitTest.dll
 else
     msbuild /p:Configuration=Debug KhivaCsharp/KhivaCsharp.sln
     mono ./KhivaCsharp/packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe ./KhivaCsharp/KhivaCSharpNUnitTest/bin/Debug/KhivaCSharpNUnitTest.dll --config=Debug
