115051476763145168ac0a8e00c65ee81cffc5e9
Nexxel <sergiogonzalezmuriel@gmail.com>
2019-04-24 11:03:18 +0200

    Changed Latex file

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'khiva::tex', u'Khiva Documentation',
+    (master_doc, 'khiva.tex', u'Khiva Documentation',
      u'Shapelets.io', 'manual'),
 ]
 
